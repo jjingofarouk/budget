950739cb7f176538bb554ed2d6d3eeea9d710efe
jjingofarouk <jjingofaroukk@gmail.com>
2025-03-14 15:59:03 -0700

    Update QuizQuestion.js

---
+++ src/components/QuizQuestion.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const QuizQuestion = () => {
+    return (
+        <div className='card p-3'>
+            <h4>What is the primary source of energy for the human body?</h4>
+        </div>
+    );
+};
+
+export default QuizQuestion;

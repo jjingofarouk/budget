ebaee7db30653c8b488160b8d0a5670239a88f64
jjingofarouk <jjingofaroukk@gmail.com>
2025-03-14 16:02:44 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,58 +1,25 @@
+// App.js
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-// Code to import Budget.js
-import Budget from './components/Budget';
-
-// Add code to import the other components here
-import Remaining from './components/Remaining';
-import ExpenseTotal from './components/ExpenseTotal';
-import ExpenseList from './components/ExpenseList';
-import ExpenseItem from './components/ExpenseItem';
-import AllocationForm from './components/AllocationForm';
-
-import { AppProvider } from './context/AppContext';
+import QuizHeader from './QuizHeader';
+import QuestionDisplay from './QuestionDisplay';
+import ScoreTracker from './ScoreTracker';
+import { questions } from './Questions'; // Importing questions from separate file
 
 const App = () => {
-    return (
-        <AppProvider>
-            <div className='container'>
-                <h1 className='mt-3'>Company's Budget Allocation</h1>
-                <div className='row mt-3'>
-                    {/* Add Budget component here */}
-                    <div className='col-sm'>
-                        <Budget />
-                    </div>
+  const containerStyle = {
+    maxWidth: '600px',
+    margin: '20px auto',
+    padding: '20px',
+    fontFamily: 'Arial, sans-serif'
+  };
 
-                    {/* Add Remaining component here */}
-                    <div className='col-sm'>
-                        <Remaining />
-                    </div>
-
-                    {/* Add ExpenseTotal component here */}
-                    <div className='col-sm'>
-                        <ExpenseTotal />
-                    </div>
-
-                    {/* Add ExpenseList component here */}
-                    <div className='col-12 mt-3'>
-                        <ExpenseList />
-                    </div>
-
-                    {/* Add ExpenseItem component here */}
-                    {/* Note: ExpenseItem might be used inside ExpenseList, but I'll include it separately for demonstration */}
-                    <div className='col-12 mt-3'>
-                        <ExpenseItem />
-                    </div>
-
-                    {/* Add AllocationForm component here */}
-                    <div className='col-12 mt-3'>
-                        <AllocationForm />
-                    </div>
-                </div>
-            </div>
-        </AppProvider>
-    );
+  return (
+    <div style={containerStyle}>
+      <QuizHeader />
+      <QuestionDisplay questions={questions} />
+      <ScoreTracker totalQuestions={questions.length} />
+    </div>
+  );
 };
 
 export default App;
